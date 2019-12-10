60bcc3fb56fec45e14444f904cb0733b66f7f7d5
davisbento <davisilva4222@gmail.com>
2019-12-09 23:52:08 -0300

    Initial commit + typescript config

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+
+    <div className='hero'>
+      <h1 className='title'>Bem-vindo ao SOU FITNESS</h1>
+    </div>
+
+    <style jsx>{`
+      .hero {
+        width: 100%;
+        color: #333;
+      }
+      .title {
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 48px;
+      }
+      .title {
+        text-align: center;
+      }
+    `}</style>
+  </div>
+);
+
+export default Home;
